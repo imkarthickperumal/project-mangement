ee17f6a6b3e2324a7d417904ebccd4e03287f2eb
imkarthi16 <karthiveera04@gmail.com>
2023-01-05 15:47:52 +0530

    React Redux Toolkit | Updating AddTask component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
-import Navbar from './components/Navbar';
+import logo from "./logo.svg";
+import "./App.css";
+import Navbar from "./components/Navbar";
+import AddTask from "./components/AddTask";
+import Container from "./../node_modules/react-bootstrap/esm/Container";
+import { Row, Col } from "react-bootstrap";
 
 function App() {
   return (
-    <>
-    <Navbar/>
-    </>
+    <Container>
+      <Navbar />
+      <Row className="justify-content-md-center">
+        <Col lg="6">
+        <AddTask />
+        </Col>
+      </Row>
+    </Container>
   );
 }
 
